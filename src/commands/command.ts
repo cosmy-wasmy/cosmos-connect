b8b43dee7afab6740067c6d57be62a4d2dd0d3f6
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2025-03-12 11:39:43 +0000

    closes #7

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -3,6 +3,7 @@ import { Configuration } from '../web/configuration';
 export class Commands {
     public static Register(context: vscode.ExtensionContext) {
         this.registerQueryTxCommand(context);
+        this.registerQueryNodeInfoCommand(context);
     }
 
     private static registerQueryTxCommand(context: vscode.ExtensionContext) {
@@ -43,6 +44,35 @@ export class Commands {
                     });
                 })
             });
+        }));
+    }
+
+    private static registerQueryNodeInfoCommand(context: vscode.ExtensionContext) {
+        context.subscriptions.push(vscode.commands.registerCommand('cosmos-connect.queryNodeInfo', () => {
+            vscode.window.withProgress({
+                location: vscode.ProgressLocation.Notification,
+                title: 'Querying node info...',
+                cancellable: false,
+            }, () => {
+                return new Promise(async (resolve, reject) => {
+                    const baseUrl = Configuration.GetChainRestUrl();
+                    const queryNodeInfoUrl = `${baseUrl}/cosmos/base/tendermint/v1beta1/node_info`;
+                    const response = await fetch(queryNodeInfoUrl);
+                    if (!response.ok) {
+                        vscode.window.showErrorMessage(`Failed to query node info: ${response.statusText}`);
+                        reject();
+                    }
+                    const data = await response.json();
+                    const display = JSON.stringify(data, null, 2);
+                    vscode.workspace.openTextDocument({
+                        language: "json",
+                        content: display
+                    }).then(doc => {
+                        vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
+                    });
+                    resolve(undefined);
+                });
+            })
         }));
     }
 }
